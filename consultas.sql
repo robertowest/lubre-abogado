6e254ae5acec2ec9811f82732bf3fb32ba9afa75
Roberto West <roberto.west@gmail.com>
2019-05-10 10:04:47 -0300

    modificaciones

--- consultas.sql
+++ consultas.sql
@@ -1,10 +1,11 @@
-create or alter view deuda_cliente_vendedor_v (idvendedor, idcliente, idenc_mov, total, comprobante, dias, meses)
-as
+CREATE OR ALTER VIEW DEUDA_CLIENTE_VENDEDOR_V (IDVENDEDOR, IDCLIENTE, IDENC_MOV, TOTAL, COMPROBANTE, FECHA, VENCE, DIAS)
+AS
 select cs.idvendedor,
 	   result.idcliente, result.idenc_mov, result.total,
 	   rpad(cc.tipocomprob, 4, ' ') || rpad(cc.letra, 2, ' ') || lpad(cc.terminal, 4, '0') || '-' || lpad(cc.numero, 8, '0') comprobante,
-     datediff(month, cc.vence, current_date) meses, datediff(day, cc.vence, current_date) dias
-from 
+	   cc.fecha, cc.fecha + pv.diascob1 vence,
+       datediff(day, cc.fecha + pv.diascob1, current_date) dias
+from
 (
   select idcliente, ref_idenc_mov as idenc_mov, sum(importe) total
   from cuentacte
@@ -13,5 +14,5 @@ from
 ) result
 inner join cuentacte cc on cc.idenc_mov = result.idenc_mov
 inner join clientessucursal cs on cs.idcliente = cc.idcliente
+inner join ptovta pv on pv.ptovta = cc.terminal
 where datediff(day, cc.vence, current_date) > 0
-
